6a5418338880b32bab8956983581bc2fc2a0b28b
ParkCheolRyeon <parkcr@i-screamarts.com>
2025-07-03 15:53:08 +0900

    배포 테스트

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function page() {
-  return <div>대시보드</div>;
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+      <div>대시보드 </div>
+      <div>배포 테스트1</div>
+    </div>
+  );
 }

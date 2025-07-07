a836c01e950875db99212d07d484dd74ea329c1b
ParkCheolRyeon <parkcr@i-screamarts.com>
2025-07-07 13:19:00 +0900

    테스트5

--- src/app/(main)/school/list/page.tsx
+++ src/app/(main)/school/list/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import SchoolList from "@/app/(main)/school/list/SchoolList";
 import { getSchoolList } from "@/app/actions/school/getSchoolListAction";
 
+export const dynamic = "force-dynamic";
 export default async function Page() {
   const res = await getSchoolList();
 

b7c71acddac8c6f041834abe817200c2a2601fda
ParkCheolRyeon <parkcr@i-screamarts.com>
2025-07-07 13:23:43 +0900

    const dynamic 테스트

--- src/app/(main)/student/list/page.tsx
+++ src/app/(main)/student/list/page.tsx
@@ -1,6 +1,7 @@
 import StudentListContainer from "@/app/(main)/student/list/StudentListContainer";
 import { getStudentList } from "@/app/actions/student/getStudentListAction";
 
+export const dynamic = "force-dynamic";
 export default async function page() {
   const studentList = await getStudentList({
     schoolId: "1ad52d51-798d-41d0-b09e-3517238fc7b7",
